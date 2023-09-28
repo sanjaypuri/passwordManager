d94a8d7a1a960716b4e23567d2f77ca52b14c376
Sanjay Puri <purisanjay007@gmail.com>
2023-09-28 14:32:57 +0530

    Origin url updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors(
   {
-    origin:["http://localhost:3000", "https://phonebookfrontend.vercel.app"],
+    origin:["http://localhost:3000", "https://password-manager-fawn.vercel.app"],
     methods:["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     credentials: true
   }
